d7e45ba96d079fb09dc5b7c35d93a5dfd4606355
Adrian Lucero <adrianrafaellucero9@gmail.com>
2021-12-03 11:14:18 +0800

    ssr

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import Vue from "@vitejs/plugin-vue";
 import Markdown from "vite-plugin-md";
 import Pages from "vite-plugin-pages";
 import WindiCSS from "vite-plugin-windicss";
-
+import SSR from "vite-ssr/plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,5 +16,6 @@ export default defineConfig({
       extensions: ["vue", "md"],
     }),
     WindiCSS(),
+    viteSSR(),
   ],
 });
